e9e25467a8adf0d5389d46b414cdd9bef733d117
Tama-chan <61571823+Uryaaa@users.noreply.github.com>
2022-09-26 15:46:21 +0800

    Delete events directory

--- events/follow.js
+++
@@ -1,6 +0,0 @@
-module.exports = (client, message) => {
-  message.reply({
-    type: "text",
-    text: `Terima kasih sudah follow saya, untuk memulai menggunakan bot, kamu harus invite saya kedalam grup, oiyaa prefix saya adalah "!"`,
-  });
-};
--- events/join.js
+++
@@ -1,6 +0,0 @@
-module.exports = (client, message) => {
-  message.reply({
-    type: "text",
-    text: `Halo halo terima kasih sudah di invite kesini\n\n prefix saya : "!"`,
-  });
-};
